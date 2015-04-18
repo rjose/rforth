edf921514620ff40a3550ac76bc93222ab24df3c
Rino Jose <rjose@spreadsheetkit.com>
2015-04-17 20:51:24 -0700

    Return index of next word from next_word

--- n-pass/pass3/next_word.s
+++ n-pass/pass3/next_word.s
@@ -14,12 +14,7 @@ input_length:
 cur_word:
 	.ascii "123456789012345678901234567\0"
 
-space:
-	.ascii " "
 
-
-
-	
 	#===============================================================================
 	# BSS section
 	#===============================================================================
@@ -58,27 +53,28 @@ read_line:
 	# If last character is a newline, replace it with a space
 	movb buffer(%eax), %bl
 	cmp $NEWLINE, %bl
-	jne return
+	jne 0f
 	movb $SPACE, %bl
 	movb %bl, buffer(%eax)
-return:
+
+0:	# Return from readline
 	ret
 
 
 	#-------------------------------------------------------------------------------
 	# Stores next word from |buffer| into |cur_word|
 	#
-	# The size of the word is returned in EAX
+	# EAX is 0 if there is no word, index of end of word in |buffer| otherwise.
 	#-------------------------------------------------------------------------------
 	.type next_word, @function
 next_word:
-	leal buffer, %edi               # buffer to scan
-	leal space, %esi                # Char to compare
-	movl input_length, %ecx         # Max num chars to scan
-	lodsb                           # Loads "space" byte into AL register
 	cld                             # Clear direction flag (increment after scan)
+	leal buffer, %edi               # buffer to scan
+	movb $SPACE, %al
+	movl input_length, %ecx         # Max num chars to scan
 	repe scasb                      # Scan for non-space
-	je non_space_not_found
+	movl $0, %eax                   # Indicate no word found
+	je 0f
 
 	# Compute start of word
 	addl $1, %ecx
@@ -93,7 +89,8 @@ next_word:
 	movl $MAX_CUR_WORD_LEN, %ecx    # Count down from MAX_CUR_WORD_LEN
 
 1:	# Loop, copying characters
-	cmp $SPACE, (%esi)
+	movb (%esi), %bl
+	cmp $SPACE, %bl
 	je 2f                           # Return
 	cmp $0, %ecx
 	je 2f                           # Return
@@ -102,12 +99,18 @@ next_word:
 	subl $1, %ecx
 	jmp 1b                          # Loop
 
-2:	# Fill rest of cur word with spaces
+2:	# Note start index of next word
+	movl %esi, %edx
+	subl $buffer, %edx
+	
+	# Fill rest of cur word with spaces
+	movb $SPACE, %al
 	rep stosb
-	ret
 
-non_space_not_found:
-	movl $0, %eax
+	# Return start index of next word
+	movl %edx, %eax
+
+0:	# Return from next_word
 	ret
 	
 
