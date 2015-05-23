b10030f1eeb3e3660f91fd2498be8c7a6cf3dcdb
Rino Jose <rjose@spreadsheetkit.com>
2015-05-23 15:32:29 -0700

    Rewrite Getc to use pointers to buffer/indexes
    	- This lets us swap buffers in/out to allow for nested file reads

--- src/gas/Getc.s
+++ src/gas/Getc.s
@@ -15,6 +15,18 @@
 .buf_index:
 	.int	MAXLINE
 
+#---------------------------------------
+# Pointers
+#---------------------------------------
+.num_chars_read_p:
+	.quad .num_chars_read
+
+.buf_index_p:
+	.quad .buf_index
+
+.buffer_p:
+	.quad buffer
+
 #===============================================================================
 # BSS section
 #===============================================================================
@@ -36,37 +48,45 @@
 # Args:
 #   * %rdi: destination for char
 #
-# NOTE: Characters are loaded into |buffer| and returned from there.
+# NOTE: Characters are loaded into a buffer and returned from there. Also, all
+#       indexes and buffers can be swapped in/out to allow "nested" file reads.
+#
+# Registers:
+#   * r11: Used to dereference pointers
 #-------------------------------------------------------------------------------
 	.globl Getc
 	.type Getc, @function
 
 Getc:
 	# If there are stil chars in the buffer, return the next one...
-	movl .buf_index, %eax
-	cmp .num_chars_read, %eax
-	jl .store_next_char
+	movq .buf_index_p, %r11         # Dereference .buf_index_p
+	movl (%r11), %eax               # to get index of next char in buffer to return
 
-	# ...otherwise, we're out of chars and need to read in another line
-	movq $SYSCALL_READ, %rax
-	MSyscall G_input_fd, $buffer, $MAXLINE
+	movq .num_chars_read_p, %r11    # Dereference .num_chars_read_p
+	cmp (%r11), %eax                # and compare it to index of next char in buffer
+	jl .store_next_char             # If there are still characters to return, do so
 
+	movq $SYSCALL_READ, %rax        # Otherwise, we need to read more into our buffer
+	MSyscall G_input_fd, .buffer_p, $MAXLINE
 
 	# Reset buf_index and num_chars_read
-	movl $0, .buf_index
-	movl %eax, .num_chars_read
+	movq .buf_index_p, %r11         # Dereference .buf_index_p so we can...
+	movl $0, (%r11)                 # ...set it to 0
 
-	# If have input, store the next char; otherwise store an EOF
-	cmp $0, .num_chars_read
-	jg .store_next_char
-	movb $ASCII_EOF, (%rdi)
-	jmp 0f	 # Return
+	movq .num_chars_read_p, %r11    # Derefernce .num_chars_read_p so we can...
+	movl %eax, (%r11)               # ...set it to the num chars we just read
+
+	cmpl $0, (%r11)                 # If we read anything
+	jg .store_next_char             # then we can return the next char
+	movb $ASCII_EOF, (%rdi)         # Otherwise, store an EOF
+	jmp 0f                          # and return
 
 .store_next_char:
-	movl .buf_index, %esi
-	movb buffer(%esi), %al
-	movb %al, (%rdi)
-	incl .buf_index
+	movq .buf_index_p, %r11         # Dereference .buf_index_p
+	movl (%r11), %esi               # and put the current index into esi.
+	movb buffer(%esi), %al          # Store that char in al
+	movb %al, (%rdi)                # and then write it to our destination
+	incl (%r11)                     # Advance our buf index
 
 0:	# Return
 	ret
