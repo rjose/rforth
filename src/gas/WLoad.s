86cb6b6136e42461a775d0e7900a7d2374daaa22
Rino Jose <rjose@spreadsheetkit.com>
2015-05-26 20:34:16 -0700

    Recomment WLoad.s

--- src/gas/WLoad.s
+++ src/gas/WLoad.s
@@ -1,6 +1,12 @@
 #===============================================================================
-# DATA section
+# WLoad.s
+#
+# This opens a file and interprets its contents.
 #===============================================================================
+
+#========================================
+# DATA section
+#========================================
 	.section .data
 	.include "./src/gas/defines.s"
 	.include "./src/gas/macros.s"
@@ -11,31 +17,36 @@
 .err_cant_close:
 	.asciz "ERROR: Can't close file"
 
-#===============================================================================
+#========================================
 # TEXT section
-#===============================================================================
+#========================================
 	.section .text
 
 
 #-------------------------------------------------------------------------------
-# WLoad - Loads and interprets an rforth file.
+# Loads and interprets an rforth file
 #
-# This assumes there's a pointer to a filename string on the forth stack.
+# Forth stack:
+#   * pointer to filename
 #-------------------------------------------------------------------------------
 	.globl WLoad
 	.type WLoad, @function
 
 WLoad:
+	pushq %rdi                      # Save caller's registers
+        pushq %rax                      # .
+	pushq %rcx                      # .
+	
 	# Open the file on the stack
 	MPop %rdi                       # Pop filename into rdi...
-	movq $SYSCALL_OPEN, %rax        # and open it read-only
-	MSyscall %rdi, $O_RDONLY, $0    # 
-	cmp $0, %rax                    # Check the return value
-	jge .set_fd                     # If > 0, we have a file descriptor
+	movq $SYSCALL_OPEN, %rax        # and
+	MSyscall %rdi, $O_RDONLY, $0    # open it read-only
+	cmp $0, %rax                    # If the return value
+	jge .push_buffer_set            # is > 0, we have a valid fd.
 	MAbort $.err_invalid_file       # Otherwise, abort
-	jmp 0f
+	jmp 0f                          # and return
 
-.set_fd:
+.push_buffer_set:
 	movl %eax, %ecx                 # Store opened fd in ecx so PushBufferSet
 	call PushBufferSet              # can use it with a fresh buffer set.
 
@@ -57,4 +68,8 @@ WLoad:
 
 0:
 	movl $0, RW_is_eof              # Clear the EOF flag before we return
+
+	popq %rcx                       # Restore caller's registers
+	popq %rax                       # .
+	popq %rdi                       # .
 	ret
