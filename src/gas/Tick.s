4ee570321381ed44f6a69c6144d4f631768885b3
Rino Jose <rjose@spreadsheetkit.com>
2015-05-26 22:09:37 -0700

    Recomment Tick.s

--- src/gas/Tick.s
+++ src/gas/Tick.s
@@ -1,13 +1,20 @@
 #===============================================================================
-# DATA section
+# Tick.s
+#
+# This defines searching for words in the dictionary. It is named after
+# forth's "`" word.
 #===============================================================================
+
+#========================================
+# DATA section
+#========================================
 	.section .data
 	.include "./src/gas/defines.s"
 	.include "./src/gas/macros.s"
 
-#===============================================================================
+#========================================
 # TEXT section
-#===============================================================================
+#========================================
 	.section .text
 
 #-------------------------------------------------------------------------------
@@ -21,40 +28,41 @@
 #-------------------------------------------------------------------------------
 	.globl Tick
 	.type Tick, @function
+
 Tick:
-	# Get word to search for
-	call ReadWord
+	pushq %rax                      # Save caller's registers
+	pushq %rbx                      # .
 
-	# Start searching at most recent dictionary entry
-	movq G_dp, %rax
+	call ReadWord                   # Read word to search for
 
+	movq G_dp, %rax                 # Start search at most recent entry
 .loop:
-	# If entry isn't zero, check it; otherwise, it's not there.
-	cmp $0, %rax
-	jne .check_entry
-	jmp 0f
+	cmp $0, %rax                    # If it's
+	jne .check_entry                # not zero, check for a match.
+	jmp 0f                          # Otherwise, we didn't find it (0 is
+	                                # the start of the dictionary).
 
 .check_entry:
-	# Compare RW_tib_count with count in entry
-	movb  RW_tib_count, %bl
-	cmp %bl, ENTRY_COUNT(%rax)
-	jne .try_previous_entry
+	movb  RW_tib_count, %bl         # Put the length of the word in bl
+	cmp %bl, ENTRY_COUNT(%rax)      # If the length of the entry word
+	jne .try_previous_entry         # doesn't match, try previous entry.
 
-	# Compare first 4 chars with entry
-	movl RW_tib, %ebx
-	cmp %ebx, ENTRY_NAME(%rax)
-	jne .try_previous_entry
+	movl RW_tib, %ebx               # Otherwise, get the first 4 chars of word
+	cmp %ebx, ENTRY_NAME(%rax)      # If the chars in the entry word
+	jne .try_previous_entry         # don't match, try previous entry.
 
-	# Otherwise, we have a match!
-	jmp 0f
+	jmp 0f                          # Otherwise, we have a match!
 
 .try_previous_entry:
-	movq ENTRY_LINK(%rax), %rbx
-	movq %rbx, %rax
-	jmp .loop
+	movq ENTRY_LINK(%rax), %rbx     # Put the link of the prev entry in rbx
+	movq %rbx, %rax                 # Copy to rax
+	jmp .loop                       # and try again
 	
-0:	# Return the entry's address
-	pushq %rax
-	call PushParam
-	MClearStackArgs 1
+0:
+	pushq %rax                      # rax has the entry's address
+	call PushParam                  # Return it via the forth stack
+	MClearStackArgs 1               # Remove stack arg
+
+	popq %rbx                       # Restore caller's registers
+	popq %rax                       # .
 	ret
