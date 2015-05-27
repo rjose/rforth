705876babaa4e95073c75b03e4267b9009d2d019
Rino Jose <rjose@spreadsheetkit.com>
2015-05-26 21:37:54 -0700

    Recomment DefineBuiltinWords.s

--- src/gas/DefineBuiltinWords.s
+++ src/gas/DefineBuiltinWords.s
@@ -1,6 +1,13 @@
 #===============================================================================
-# DATA section
+# DefineBuiltinWords.s
+#
+# This defines all of the builtin words that the rforth interpreter can
+# run without external files.
 #===============================================================================
+
+#========================================
+# DATA section
+#========================================
 	.section .data
 	.include "./src/gas/defines.s"
 	.include "./src/gas/macros.s"
@@ -120,14 +127,14 @@
 	.ascii "OVER"
 	.equ LEN_OVER, 4
 
-#===============================================================================
+#========================================
 # TEXT section
-#===============================================================================
+#========================================
 	.section .text
 
 
 #-------------------------------------------------------------------------------
-# DefineBuiltinWords - Defines words for math functions
+# DefineBuiltinWords
 #-------------------------------------------------------------------------------
 	.globl DefineBuiltinWords
 	.type DefineBuiltinWords, @function
@@ -139,35 +146,45 @@ DefineBuiltinWords:
 	MDefineWord .name_star, $LEN_STAR, WStar
 	MDefineWord .name_slash, $LEN_SLASH, WSlash
 
+	# Define "CONSTANT", "VARIABLE", "!", "@"
 	MDefineWord .name_CONSTANT, $LEN_CONSTANT, WConstant
 	MDefineWord .name_VARIABLE, $LEN_VARIABLE, WVariable
 	MDefineWord .name_BANG, $LEN_BANG, WBang
 	MDefineWord .name_AT, $LEN_AT, WAt
 
+	# Define "#"
 	MDefineWord .name_HASH, $LEN_HASH, WHash
 
+	# Define ".s", ".q"
 	MDefineWord .name_dot_s, $LEN_DOT_S, WDotS
 	MDefineWord .name_dot_q, $LEN_DOT_Q, WDotQ
 
-	MDefineImmediateWord .name_semicolon, $LEN_SEMICOLON, WSemicolon
+	# Define ":", ";"
 	MDefineWord .name_colon, $LEN_COLON, WColon
+	MDefineImmediateWord .name_semicolon, $LEN_SEMICOLON, WSemicolon
 
+	# Define "IF", "ELSE", "THEN"
 	MDefineImmediateWord .name_if, $LEN_IF, WIf
 	MDefineImmediateWord .name_then, $LEN_THEN, WThen
 	MDefineImmediateWord .name_else, $LEN_ELSE, WElse
 
+	# Define "WHILE", "REPEAT"
 	MDefineImmediateWord .name_while, $LEN_WHILE, WWhile
 	MDefineImmediateWord .name_repeat, $LEN_REPEAT, WRepeat
 
+	# Define '."', "LOAD"
 	MDefineWord .name_dot_quote, $LEN_DOT_QUOTE, WDotQuote
 	MDefineWord .name_load, $LEN_LOAD, WLoad
 
+	# Define "INTERPRET"
 	MDefineWord .name_interpret, $LEN_INTERPRET, Interpret
 
+	# Define "PRINT", "ABORT", "RESET"
 	MDefineWord .name_print, $LEN_PRINT, ForthPrint
 	MDefineWord .name_abort, $LEN_ABORT, WAbort
 	MDefineWord .name_reset, $LEN_RESET, Reset
 
+	# Define "DUP", "SWAP", "DROP", "OVER"
 	MDefineWord .name_dup, $LEN_DUP, Dup
 	MDefineWord .name_swap, $LEN_SWAP, Swap
 	MDefineWord .name_drop, $LEN_DROP, DropParam
