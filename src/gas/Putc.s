542a8eca797453d7707fdeb9460cf57644bd4d34
Rino Jose <rjose@spreadsheetkit.com>
2015-05-26 21:54:11 -0700

    Recomment Putc.s

--- src/gas/Putc.s
+++ src/gas/Putc.s
@@ -1,29 +1,34 @@
 #===============================================================================
-# DATA section
+# Putc.s
+#
+# This defines buffered writes to STDOUT.
 #===============================================================================
+
+#========================================
+# DATA section
+#========================================
 	.section .data
 	.include "./src/gas/defines.s"
 	.include "./src/gas/macros.s"
 
-	.equ	 MAXLINE, 256
+	.equ	 MAXLINE, 256           # Buffer size
 	.equ	 STDOUT, 1
 
 # Index of the next open slot in tob
 .buf_index:
 	.quad	0
 
-#===============================================================================
+#========================================
 # BSS section
-#===============================================================================
+#========================================
 	.section .bss
 
-	# "text output buffer" to hold characters buffer flushing them out
-	.lcomm tob, MAXLINE
+	.lcomm tob, MAXLINE             # "text output buffer"
 
 
-#===============================================================================
+#========================================
 # TEXT section
-#===============================================================================
+#========================================
 	.section .text
 
 #-------------------------------------------------------------------------------
@@ -33,36 +38,38 @@
 	.type Flush, @function
 
 Flush:
-	movq $SYSCALL_WRITE, %rax
+	pushq %rax                      # Save caller's registers
+	
+	movq $SYSCALL_WRITE, %rax       # Write our buffer to STDOUT
 	MSyscall $STDOUT, $tob, .buf_index
 
-	# Reset the index
-	movq $0, .buf_index
+	movq $0, .buf_index             # Reset our buffer index
+
+	popq %rax                       # Restore caller's registers
 	ret
 
 
 #-------------------------------------------------------------------------------
 # Writes char to output buffer
 #
-# Args:
+# Register args:
 #   * %al: char to write
 #-------------------------------------------------------------------------------
 	.globl Putc
 	.type Putc, @function
 
 Putc:
-	# If |tob| has space, add char to buffer
-	movq .buf_index, %rbx
-	cmp $MAXLINE, %rbx
-	jl .add_char_to_tob
+	pushq %rdi                      # Save caller's registers
 
-	# Otherwise, flush first.
-	call Flush
+	movq .buf_index, %rdi           # Put buf index into rdi
+	cmp $MAXLINE, %rdi              # and compare it to MAXLINE
+	jl .add_char_to_tob             # if there's room, add a char to the buffer.
+	call Flush                      # Otherwise, flush the buffer first.
 
 .add_char_to_tob:
-	movq .buf_index, %rdi
-	movb %al, tob(%rdi)
-	incq .buf_index
+	movb %al, tob(%rdi)             # Put char into next buffer slot
+	incq .buf_index                 # and advance buf index
 
 0:
+	popq %rdi                       # Restore caller's registers
 	ret
