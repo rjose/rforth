f44998aeb950c42e45b6815a460f8d39f46cc68e
Rino Jose <rjose@spreadsheetkit.com>
2015-05-27 22:22:46 -0700

    Recomment PushEntryParam.s

--- src/gas/PushEntryParam.s
+++ src/gas/PushEntryParam.s
@@ -1,13 +1,20 @@
 #===============================================================================
-# DATA section
+# PushEntryParam.s
+#
+# This implements functions that push paramters values and addresses onto
+# the forth stack.
 #===============================================================================
+
+#========================================
+# DATA section
+#========================================
 	.section .data
 	.include "./src/gas/defines.s"
 	.include "./src/gas/macros.s"
 
-#===============================================================================
+#========================================
 # TEXT section
-#===============================================================================
+#========================================
 	.section .text
 
 
@@ -22,10 +29,16 @@
 
 PushEntryParam1:
 	MPrologue
+	
+	pushq %rax                      # Save caller's registers
+	pushq %rbx                      # .
 
-	movq STACK_ARG_1(%rbp), %rbx            # Dictionary entry
-	movq ENTRY_PFA(%rbx), %rax              # Param1 value
+	movq STACK_ARG_1(%rbp), %rbx    # Dictionary entry
+	movq ENTRY_PFA(%rbx), %rax      # Param1 value
 	MPush %rax
+
+	popq %rbx                       # Restore caller's registers
+	popq %rax                       # .
 
 	MEpilogue
 	ret
@@ -42,9 +55,13 @@ PushEntryParam1:
 PushEntryParam1Addr:
 	MPrologue
 
+	pushq %rbx                      # Save caller's registers
+
 	movq STACK_ARG_1(%rbp), %rbx    # Address of word
 	addq $ENTRY_PFA, %rbx           # Address of param1
 	MPush %rbx                      # Push address onto stack
+
+	popq %rbx                       # Restore caller's registers
 
 	MEpilogue
 	ret
