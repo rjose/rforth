7a49b93cea8131ee457d4d7be11631ed5f0f9534
Rino Jose <rjose@spreadsheetkit.com>
2015-05-27 20:00:30 -0700

    Recomment WThen.s

--- src/gas/WThen.s
+++ src/gas/WThen.s
@@ -1,13 +1,20 @@
 #===============================================================================
-# DATA section
+# WThen.s
+#
+# Defines the immediate THEN word which figures out where the last
+# conditional jump should go to (right after the THEN).
 #===============================================================================
+
+#========================================
+# DATA section
+#========================================
 	.section .data
 	.include "./src/gas/defines.s"
 	.include "./src/gas/macros.s"
 
-#===============================================================================
+#========================================
 # TEXT section
-#===============================================================================
+#========================================
 	.section .text
 
 
@@ -23,11 +30,19 @@
 	.type WThen, @function
 
 WThen:
+	pushq %rax                      # Store caller's registers
+	pushq %rbx                      # .
+	pushq %rcx                      # .
+
 	MPop %rbx                       # Index of the param to fill out
 	movq G_dp, %rax                 # G_dp is the colon def we're currently in
 
 	movq G_param_index, %rcx        # Fill out dangling Jmp_false slot
 	movq %rcx, ENTRY_PFA(%rax, %rbx, WORD_SIZE)
+
+	popq %rcx                       # Restore caller's registers
+	popq %rbx                       # .
+	popq %rax                       # .
 
 .done:
 	ret
