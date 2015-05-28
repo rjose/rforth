8d41b455ca97280a8d8a0e1d9474aa16fc84da9b
Rino Jose <rjose@spreadsheetkit.com>
2015-05-27 20:50:59 -0700

    Recomment WColon.s

--- src/gas/WColon.s
+++ src/gas/WColon.s
@@ -1,13 +1,19 @@
 #===============================================================================
-# DATA section
+# WColon.s
+#
+# Defines word to define colon definitions.
 #===============================================================================
+
+#========================================
+# DATA section
+#========================================
 	.section .data
 	.include "./src/gas/defines.s"
 	.include "./src/gas/macros.s"
 
-#===============================================================================
+#========================================
 # TEXT section
-#===============================================================================
+#========================================
 	.section .text
 
 
@@ -18,30 +24,25 @@
 	.type WColon, @function
 
 WColon:
-	# Create a new dictionary entry
-	call Create
+	call Create                     # Create a dictionary entry
 
-	# Store ExecuteColonDefinition in code pointer
-	lea ExecuteColonDefinition, %rbx
-	movq G_dp, %rax
-	movq %rbx, ENTRY_CODE(%rax)
+	lea ExecuteColonDefinition, %rbx  # Get function to execute a colon def
+	movq G_dp, %rax                 # G_dp has address of most recent entry
+	movq %rbx, ENTRY_CODE(%rax)     # Store the function in the entry's code slot
 
-	# We start off in non-macro (i.e, normal) mode
-	movb $0, WC_macro_mode
+	movb $0, WC_macro_mode          # Clear macro mode flag (normal case)
 
 .loop:
-	call WCompile
+	call WCompile                   # Compile next word of the definition
 
-	# If the last parameter added was an Exit_rt, then we're done.
-	movq G_pfa, %rdx
-	movq -WORD_SIZE(%rdx), %rbx
+	# Check for Exit_rt
+	movq G_pfa, %rdx                # Put G_pfa in rdx
+	movq -WORD_SIZE(%rdx), %rbx     # and write the most recent param to rbx.
+	lea Exit_rt, %rcx               # Put the address of Exit_rt in rcx
+	cmp %rbx, %rcx                  # If the most recent param is an Exit_rt then
+	je .done                        # we're done with this definition.
 
-	lea Exit_rt, %rcx
-	cmp %rbx, %rcx
-	je .done
-
-	# Otherwise, keep going
-	jmp .loop
+	jmp .loop                       # Otherwise, repeat
 
 .done:
 	ret
