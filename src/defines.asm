e01de57fa538bae00bb498bcb54857b6e4f90459
Rino Jose <rjose@spreadsheetkit.com>
2015-06-20 16:44:39 -0700

    Run loop with a nanosleep call

--- src/defines.asm
+++ src/defines.asm
@@ -3,5 +3,6 @@ STDOUT: equ 1
 ;;; ====================================
 ;;; Instruction codes
 ;;; ====================================
-SYSCALL_EXIT:	equ 60
-SYSCALL_WRITE:	equ 1
+SYSCALL_EXIT:           equ 60
+SYSCALL_WRITE:          equ 1
+SYSCALL_NANOSLEEP:      equ 35
