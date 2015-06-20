0862dc4a069d17370e125ed8a4425a2c69dd7bac
Rino Jose <rjose@spreadsheetkit.com>
2015-06-20 07:22:43 -0700

    Add defines.asm

---
+++ src/defines.asm
@@ -0,0 +1,7 @@
+STDOUT: equ 1
+
+;;; ====================================
+;;; Instruction codes
+;;; ====================================
+SYSCALL_EXIT:	equ 60
+SYSCALL_WRITE:	equ 1
--- src/rforth.asm
+++ src/rforth.asm
@@ -1,14 +1,27 @@
-	section .data
-	section .text
+;;; ===============================================================================
+;;; rforth.asm
+;;; ===============================================================================
 
+	SECTION .data
+	%include "./src/defines.asm"
+
+Message:	db "Howdy", 10  ; Message + newline
+MessageLen:	equ $ - Message
+	
+	SECTION .text
 	global main
 
 main:
 	nop
-	;; Put experiments here
-	mov rax, 60		; EXIT code
-	mov rdi, 42
-	syscall
+	mov rax, SYSCALL_WRITE  ; Instruction: WRITE
+	mov rdi, STDOUT         ; Output file descriptor
+	mov rsi, Message        ; Address of message
+	mov rdx, MessageLen     ; Length of message
+	syscall                 ; Call WRITE
+
+	mov rax, SYSCALL_EXIT	; Instruction: EXIT
+	mov rdi, 42             ; Status code
+	syscall                 ; Call EXIT
 	nop
 
-	section .bss
+	SECTION .bss
