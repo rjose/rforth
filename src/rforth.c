7b7c45114d03f2739bd1c824a6787a61db6b89e8
Rino Jose <rjose@spreadsheetkit.com>
2015-06-20 19:38:05 -0700

    Clean up whitespace

--- src/rforth.c
+++ src/rforth.c
@@ -6,31 +6,31 @@
 
 static void
 handler(int sig) {
-  printf("Got signal %d\n", sig);
-  exit(0);
+    printf("Got signal %d\n", sig);
+    exit(0);
 }
 
 int
 main(int argc, char* argv[]) {
-  int status = 0;
+    int status = 0;
 
-  struct sigaction sa;
-  sigemptyset(&sa.sa_mask);
-  sa.sa_handler = handler;
-  if(sigaction(SIGINT, &sa, NULL) == -1) {
-    printf("Ugh. sigaction failed\n");
-    exit(2);
-  }
-  
-  struct timespec requested;
-  while(1) {
-    printf("Howdy!\n");
-    requested.tv_sec = 0;
-    requested.tv_nsec = 500000000; // 500 ms
-    status = nanosleep(&requested, NULL);
-    if (status == -1) {
-      printf("Interrupted by %d\n", errno);
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_handler = handler;
+    if(sigaction(SIGINT, &sa, NULL) == -1) {
+        printf("Ugh. sigaction failed\n");
+        exit(2);
     }
-  }
-  return 0;
+
+    struct timespec requested;
+    while(1) {
+        printf("Howdy!\n");
+        requested.tv_sec = 0;
+        requested.tv_nsec = 500000000; // 500 ms
+        status = nanosleep(&requested, NULL);
+        if (status == -1) {
+            printf("Interrupted by %d\n", errno);
+        }
+    }
+    return 0;
 }
